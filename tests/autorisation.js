6d9b0837d0e63af84df6d13ae3f062e40e826272
JaneGame <fibi92@mail.ru>
2022-05-19 21:04:38 +0300

    tests error

--- tests/autorisation.js
+++ tests/autorisation.js
@@ -1,15 +1,32 @@
 const {username, password} = require('../users.js');
 const {chromium} = require('playwright');
+const { test, expect } = require('@playwright/test');
 
-(async() => {
+
+test('goodAutorisation', async() => {
   const browser = await chromium.launch({headless: false, slowMo: 5000});
   const page = await browser.newPage();
-  await page.goto('https://netology.ru/?modal=sign_in');
-  await page.fill('[placeholder=\'Email\']', username);
-  await page.fill('[placeholder=\'Пароль\']', password);
-  page.click('//button[text()=\'Войти\']');
-  await expect(
-    page.locator('.components-pages-Profile-Programs--title--NCjbp')
-  ).toHaveText('Мои курсы и профессии');
+  await page.goto('https://netology.ru/?modal=sign_in', {timeout: 60000});
+  await page.pause();
+  
+  // Click [placeholder="Email"]
+  await page.locator('[placeholder="Email"]').click();
+
+  // Fill [placeholder="Email"]
+  await page.locator('[placeholder="Email"]').fill(username);
+
+  // Click [placeholder="Пароль"]
+  await page.locator('[placeholder="Пароль"]').click();
+
+  // Fill [placeholder="Пароль"]
+  await page.locator('[placeholder="Пароль"]').fill(password);
+
+  // Click [data-testid="login-submit-btn"]
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'https://netology.ru/profile' }*/),
+    page.locator('[data-testid="login-submit-btn"]').click()
+  ]);
+
+  await expect().locator('[data-testid="menu-userface"] div');
   await browser.close();
-})();
+});
